954de79c627a8799eb1ea2c60b206f62fa6e55bb
Philippe Luchansky <philuchansky@gmail.com>
2017-01-10 14:10:55 -0800

    sample response comments in controller

--- controllers/haikus.js
+++ controllers/haikus.js
@@ -4,31 +4,31 @@ var
 module.exports = {
   index: (req, res) => {
     Haiku.find({}, (err, haikus) => {
-      res.json(haikus)
+      // respond here
     })
   },
 
   show: (req, res) => {
     Haiku.findById(req.params.id, (err, haiku) => {
-      //
+      // respond here
     })
   },
 
   create: (req, res) => {
     Haiku.create(req.body, (err, haiku) => {
-
+      // respond here
     })
   },
 
   update: (req, res) => {
     Haiku.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, haiku) => {
-
+      // respond here
     })
   },
 
   destroy: (req, res) => {
     Haiku.findByIdAndRemove(req.params.id, (err, haiku) => {
-
+      // respond here
     })
   }
 }
